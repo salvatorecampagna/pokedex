2706ed28a2aee574e143320ae85ee8aa10163440
Salvatore Campagna <salvatorecampagna@gmail.com>
2021-07-18 11:30:07 +0200

    Add translation object entities

---
+++ src/main/java/com/trustlayer/pokedex/translate/model/TranslationResponse.java
@@ -0,0 +1,37 @@
+package com.trustlayer.pokedex.translate.model;
+
+import java.util.Objects;
+
+public class TranslationResponse {
+    private Success success;
+    private Contents contents;
+
+    public TranslationResponse() {
+    }
+
+    public Success getSuccess() {
+        return success;
+    }
+
+    public Contents getContents() {
+        return contents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TranslationResponse that = (TranslationResponse) o;
+        return Objects.equals(success, that.success) && Objects.equals(contents, that.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(success, contents);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TranslationResponse{success=%s, contents=%s}", success, contents);
+    }
+}
