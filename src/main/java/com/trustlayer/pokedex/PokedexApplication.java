e59835fadbef9b4da34579ac4334d50eb9975162
Salvatore Campagna <salvatorecampagna@gmail.com>
2021-07-18 11:25:29 +0200

    Initialize git repository with application skeleton

---
+++ src/main/java/com/trustlayer/pokedex/PokedexApplication.java
@@ -0,0 +1,13 @@
+package com.trustlayer.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApplication.class, args);
+	}
+
+}
