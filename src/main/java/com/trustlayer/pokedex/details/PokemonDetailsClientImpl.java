b4ee36edaa8a11a3558256fa3e71d49c828bfb7f
Salvatore Campagna <salvatorecampagna@gmail.com>
2021-07-18 15:09:25 +0200

    Use Hystrix circuit breaker on pokemon rest api call
    Calling a REST API might fail for different reasons, including
    network issues. Here we use Hystrix as a circuit breaker to
    protect ourself against both complete failure and latency issues.

--- src/main/java/com/trustlayer/pokedex/details/PokemonDetailsClientImpl.java
+++ src/main/java/com/trustlayer/pokedex/details/PokemonDetailsClientImpl.java
@@ -1,5 +1,7 @@
 package com.trustlayer.pokedex.details;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.trustlayer.pokedex.details.model.PokemonDetails;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,12 @@ public class PokemonDetailsClientImpl implements PokemonDetailsClient {
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "getByIdOrNameFallback",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+            }
+    )
     public PokemonDetails getByIdOrName(String idOrName) {
         final String url = String.format(URL, idOrName);
         final ResponseEntity<PokemonDetails> response = restTemplate.getForEntity(url, PokemonDetails.class);
@@ -29,5 +37,11 @@ public class PokemonDetailsClientImpl implements PokemonDetailsClient {
         }
 
         return response.getBody();
+    }
+
+    public PokemonDetails getByIdOrNameFallback(String idOrName) {
+        throw new PokemonDetailsException(
+                String.format("Error while invoking the pokemon details service for pokemon '%s'", idOrName)
+        );
     }
 }
