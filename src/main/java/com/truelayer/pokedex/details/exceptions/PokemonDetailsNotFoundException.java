f906af39fb01d8304db8246ee744528ac7bd06cd
Salvatore Campagna <salvatorecampagna@gmail.com>
2021-07-21 19:13:38 +0200

    Add a more descriptive error message to HTTP 404 errors

--- src/main/java/com/truelayer/pokedex/details/exceptions/PokemonDetailsNotFoundException.java
+++ src/main/java/com/truelayer/pokedex/details/exceptions/PokemonDetailsNotFoundException.java
@@ -3,7 +3,7 @@ package com.truelayer.pokedex.details.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Pokemon not found")
 public class PokemonDetailsNotFoundException extends RuntimeException {
 
     public PokemonDetailsNotFoundException(final Throwable e) {
